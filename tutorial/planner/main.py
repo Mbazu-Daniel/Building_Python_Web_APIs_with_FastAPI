d5ee042d7f3d66d9d986197110998023b1a86001
Mbazu Daniel <mbazudaniel97@gmail.com>
2022-09-01 18:08:59 +0100

    Incorporate an SQL Database and implement CRUD operations

--- tutorial/planner/main.py
+++ tutorial/planner/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi_offline import FastAPIOffline
+from database.connection import conn
 
 from routes.users import user_router
 from routes.events import event_router
@@ -10,6 +11,10 @@ app = FastAPIOffline()
 
 app.include_router(user_router, prefix="/user")
 app.include_router(event_router, prefix="/event")
+
+@app.on_event("startup")
+def on_startup():
+    conn()
 
 
 if __name__ == "__main__":
